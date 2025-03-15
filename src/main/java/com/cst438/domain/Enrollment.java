9810018ce421bdba761dc1f470875386d7bafebd
Lupita Sanchez Ortiz <lupsanchez@csumb.edu>
2025-03-15 11:45:40 -0700

    Added constructor, fixed setter, and added toString()

--- src/main/java/com/cst438/domain/Enrollment.java
+++ src/main/java/com/cst438/domain/Enrollment.java
@@ -20,23 +20,35 @@ public class Enrollment {
     @JoinColumn(name="user_id",nullable=false)
     private User user;
 
+    public Enrollment() {}  // Default constructor
+
+    public Enrollment(User user, Section section, String grade) {
+        this.user = user;
+        this.section = section;
+        this.grade = grade;
+    }
+
     public int getEnrollmentId() {return enrollmentId;}
 
     public void setEnrollmentId(int enrollmentId) {this.enrollmentId = enrollmentId;}
 
     public String getGrade() {return grade;}
 
-    public void setGrade(String finalGrade) {this.grade = grade;}
+    public void setGrade(String finalGrade) {this.grade = finalGrade;}
 
     public Section getSection() {return section;}
 
     public void setSection(Section section) {this.section = section;}
 
-
     public User getUser() {return user;}
 
     public void setUser(User user) {this.user = user;}
 
+    //for debugging
+    @Override
+    public String toString() {
+        return "Enrollment [enrollmentId=" + enrollmentId + ", grade=" + grade + ", section=" + section + ", user=" + user + "]";
+    }
 
 	// TODO complete this class
     // add additional attribute for grade
